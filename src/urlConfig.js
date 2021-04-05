06289c3d99079c46602100cae5ae5c482783f40b
prachi <prachigoyal59@gmail.com>
2021-04-05 19:04:14 +0530

    init commit

---
+++ src/urlConfig.js
@@ -0,0 +1,9 @@
+// const baseUrl = process.env.API || "https://flipkart-rest-server.herokuapp.com";
+// const baseUrl = "http://localhost:2000";
+const baseUrl = "https://hackerramp-backend.herokuapp.com/";
+
+export const api = `${baseUrl}/api`;
+
+export const generatePublicUrl = (fileName) => {
+  return `${baseUrl}/public/${fileName}`;
+};
